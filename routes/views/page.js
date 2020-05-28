906a70a165dcd24fd740c3313d2661f388b99641
Janik Hotz <janikhotz@gmail.com>
2020-05-28 18:18:35 +0200

    cookie consent

--- routes/views/page.js
+++ routes/views/page.js
@@ -1,19 +1,19 @@
-const keystone = require("keystone");
+const keystone = require('keystone')
 
 exports = module.exports = async (req, res) => {
-	const view = new keystone.View(req, res);
+	const view = new keystone.View(req, res)
 
 	let page = await keystone
-		.list("Page")
-		.model.findOne({ slug: req.params.slug });
+		.list('Page')
+		.model.findOne({ slug: req.params.slug })
 
-	console.log("slug", req.params.slug);
-	console.log("page", page);
+	console.log('slug', req.params.slug)
+	console.log('page', page)
 
 	if (!page) {
-		return view.render("errors/404");
+		return view.render('errors/404')
 	}
-	res.locals.page = page;
+	res.locals.page = page
 
-	view.render("page");
-};
+	view.render('page')
+}
